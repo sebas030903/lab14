9fb8f037cf5637e0e3ef9955a4327e2b7b012654
sebas030903 <rodrigo.gutierrez.v@tecsup.edu.pe>
2025-06-23 21:18:43 -0500

    SimpleWidget.kt y simple_widget_info.xml

---
+++ app/src/main/java/com/example/lab14/SimpleWidget.kt
@@ -0,0 +1,6 @@
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+
+class SimpleWidget : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget = SimpleWidgetContent()
+}
